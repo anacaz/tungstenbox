ad46b66fb69f8ff220403af2f3eebbea38392051
Rob Abrams <rabrams@actmobile.com>
2014-08-20 09:40:33 -0700

    Adding.

---
+++ platform/include/w.h
@@ -0,0 +1,11 @@
+#ifndef _w_h
+#define _w_h
+/*
+ * TungstenBox(tm) the original!
+ *
+ * Copyright(c) 2014 Anacaz Networks, Inc., ALL RIGHTS RESERVED.
+ *
+ * rfa - 140820
+ */
+
+#endif /* !_w_h */
