47731297cdbd63920c8bf9941107932831acef41
Rob Abrams <rabrams@actmobile.com>
2014-08-21 15:53:22 -0700

    Fixed threading bug.

--- platform/sys/main.c
+++ platform/sys/main.c
@@ -20,20 +20,24 @@ int main(int argc, char **argv)
 {
 	int mbox1, mbox2, mbox3, mbox4, mbox5;;
 	static pthread_t t1, t2, t3, t4, t5;
+	volatile int x;
 
 	main_init();
 
 printf("CREATE ...\n");
-	// mbox1 = mbox_create("thread1", get_owner(), 0);
 	pthread_create(&t1, 0, main_thread, (void *)&mbox1);
-	// mbox2 = mbox_create("thread2", get_owner(), 0);
 	pthread_create(&t2, 0, main_thread, (void *)&mbox2);
-	mbox3 = mbox_create("thread2", get_owner(), 0);
-	mbox4 = mbox_create("thread3", get_owner(), 0);
-	mbox5 = mbox_create("thread4", get_owner(), 0);
+	pthread_create(&t3, 0, main_thread, (void *)&mbox3);
+	pthread_create(&t4, 0, main_thread, (void *)&mbox4);
+	pthread_create(&t5, 0, main_thread, (void *)&mbox5);
+
+	for (x = 0; x < 1000; ++x) ;
 
 	mbox_show();
+
 printf("mbox1=%d, mbox2=%d mbox3=%d mbox4=%d mbox5=%d\n", mbox1, mbox2, mbox3, mbox4, mbox5);
+
+	for (x = 0; x < 1000; ++x) ;
 
 	main_delete(mbox3);
 	main_delete(mbox2);
@@ -53,12 +57,11 @@ printf("mbox1=%d, mbox2=%d mbox3=%d mbox4=%d mbox5=%d\n", mbox1, mbox2, mbox3, m
 
 void *main_thread(void *arg)
 {
-	static int instance = 0;
-	char name[32];			/* BUG!!! */
+	char name[64];
 
-	sprintf(name, "%s%d", "thread", ++instance);
+	sprintf(name, "%s-%d", "thread", rand());
 	*(int *)arg = mbox_create(name, get_owner(), 0);
-	printf("Hello World %s\n", name);
+	printf("\nHello World %s\n", name);
 
 	return(0);
 }
