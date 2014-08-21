92b3b8afb2c4b5f8a006b90a6748360b7bd86860
Rob Abrams <rabrams@actmobile.com>
2014-08-21 16:29:26 -0700

    Adding some mfg info.

--- platform/include/w.h
+++ platform/include/w.h
@@ -5,7 +5,18 @@
  *
  * Copyright(c) 2014 Anacaz Networks, Inc., ALL RIGHTS RESERVED.
  *
- * rfa - 140820
+ * rfa - 140820-21
  */
+
+/*
+ * This might need to be ASCII only.
+ */
+struct mfginfo
+{
+	char		serial[128];
+	unsigned char	macadr[6];
+	unsigned char	rsakey[4096/8];			/* BUG!!! This is only one key!!! */
+	time_t		builddate;
+};
 
 #endif /* !_w_h */
