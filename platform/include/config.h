6c4f248597aa42b587d18b7ab2eef11b1deb63bf
Rob Abrams <rabrams@actmobile.com>
2014-08-22 10:34:53 -0700

    Adding config.h

---
+++ platform/include/config.h
@@ -0,0 +1,13 @@
+#ifndef _config_h
+#define _config_h
+/*
+ * System configuration options.
+ *
+ * Copyright(c) 2014 Anacaz Networks, Inc., ALL RIGHTS RESERVERED.
+ *
+ * rfa - 140822
+ */
+
+#define CONFIG_THREADMAX	256
+
+#endif /* !_config_h */
